fcb11187ad569e2e65f804bb446c6784424cb386
iamishant <65083288+iamishant@users.noreply.github.com>
2020-05-09 22:21:26 +0530

    Add files via upload

---
+++ inc/banner.inc.php
@@ -0,0 +1,2 @@
+<h1><a href="">Home<span> Tutor</span></a></h1>
+<h3><a href="#"></a></h3>
---
+++ inc/footer.inc.php
@@ -0,0 +1,26 @@
+
+	<div class="footerMain">
+		<div class="content">
+			<div class="footer-grids">
+				<div class="footer one">
+
+					<div class="clear"></div>
+				</div>
+				<div class="footer two">
+					
+					<ul>
+						<li><a class="fb" href=""><i></i>Facebook</a></li>
+						<li><a class="fb1" href=""><i></i> Youtube</a></li>
+					</ul>
+				</div>
+				<div class="footer three">
+					<h3>Contact Information</h3>
+					<ul>
+						<li></li>
+						<li><a href="mailto:info@example.com"></a></li>
+					</ul>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
